cb5f4a092a96bd4d325813fe1fa1b52f6c407f5e
Aashish Kumar <aashishkumar3105@gmail.com>
2026-06-22 01:04:40 +0530

    changed deployement

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,6 +10,8 @@ app = FastAPI(title="AI HR Automation Platform")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://127.0.0.1:3000",
+                   "https://ai-powered-hr.vercel.app/",
+                     "https://ai-powered-hr.onrender.com/"
                    ],
     allow_credentials=True,
     allow_methods=["*"],
